0298c8d71e2c7b92ba03624113b1d2e2b9df0bc5
Java Student PC-3 <islambithy606@gmail.com>
2025-02-19 12:46:55 +0600

    s

--- Angular/LocalStorageCRUDE/src/app/createuser/createuser.component.ts
+++ Angular/LocalStorageCRUDE/src/app/createuser/createuser.component.ts
@@ -1,31 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../app.component';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-createuser',
   imports: [FormsModule],
   templateUrl: './createuser.component.html',
   styleUrl: './createuser.component.css'
 })
-export class CreateuserComponent implements OnInit{
+ export class CreateuserComponent {
 u: User = new User('', 0, '', '');
-  constructor() {}
-  ngOnInit():void{
-     console.log('CreateUserComponent');
-    console.error('dummy error');
-  
-    localStorage.setItem('name','john');
-    localStorage.setItem('age','22');
-    localStorage.setItem('email','bgmail.com');
-    localStorage.setItem('name','john');
-  }
-  onSubmit(){
-    let users = JSON.parse(localStorage.getItem('user') || '[]');
-    users.push(this.u);
-    localStorage.setItem('user', JSON.stringify(users));
-   
-    this.u = new User('', 0, '', '');
-    alert('User added successfully');
-  }
+isUpdate = false;
+  router: any;
+
+constructor(router: Router) {
+  const nav = router.getCurrentNavigation();
+  if (nav?.extras.state && nav.extras.state.['user']) {
+    this.u = nav.extras.state['user'];
+    this.isUpdate = true;
+  } 
 }
+onSubmit() {
+  let users: User[] = JSON.parse(localStorage.getItem('user') || '[]');
+  if (this.isUpdate) {
+    users = users.map(user => (user.email === this.u.email ? this.u : user));
+  } else {
+    users.push(this.u);
+  }
+  localStorage.setItem('user', JSON.stringify(users));
+  this.u = new User('', 0, '', '');
+  // alert('User added successfully');
+  this.router.navigate(['/list']);
+ }
+}
+//  implements OnInit
+// u: User = new User('', 0, '', '');
+//   constructor() {}
+//   ngOnInit():void{
+//      console.log('CreateUserComponent');
+//     console.error('dummy error');
+  
+//     localStorage.setItem('name','john');
+//     localStorage.setItem('age','22');
+//     localStorage.setItem('email','bgmail.com');
+//     localStorage.setItem('name','john');
+//   }
+//   onSubmit(){
+//     let users = JSON.parse(localStorage.getItem('user') || '[]');
+//     users.push(this.u);
+//     localStorage.setItem('user', JSON.stringify(users));
+   
+//     this.u = new User('', 0, '', '');
+//     alert('User added successfully');
+//   }
+// }
 
--- Angular/LocalStorageCRUDE/src/app/list/list.component.ts
+++ Angular/LocalStorageCRUDE/src/app/list/list.component.ts
@@ -1,20 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { User } from '../app.component';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-list',
   imports: [],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
- export class ListComponent {//implements OnInit{
-// constructor() {}
-// ngOnInit():void{
-//   console.log('CreateUserComponent');
-//   console.error('dummy error');
+export class ListComponent implements OnInit {
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
 
-//   localStorage.setItem('name','john');
-//   localStorage.setItem('age','22');
-//   localStorage.setItem('email','bgmail.com');
-//   localStorage.setItem('name','john');
-// }
+  users: User[] = [];
+
+
+
+  ngOnInit() {
+    let user = JSON.parse(localStorage.getItem('user') || '[]');
+    this.users = user;
+  }
+
+  constructor(private router: Router) { }
+
+  editUser(user: User) {
+    this.router.navigate(['/create'], { state: { user } });
+  }
+
+  deleteUser(user: User) {
+    if (confirm('Are you sure you want to delete this user?')) {
+      this.users = this.users.filter(u => u !== user);
+      localStorage.setItem('user', JSON.stringify(this.users));
+    }
+  }
 }

0c73b6121f8fd49e3738640646cb114056ad93d9
Java Student PC-3 <islambithy606@gmail.com>
2025-02-26 13:14:29 +0600

    harry-potter

--- producttable/src/app/app.routes.ts
+++ producttable/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { HeaderComponent } from './header/header.component';
 import { FormComponent } from './form/form.component';
+import { HarryPotterComponent } from './harry-potter/harry-potter.component';
 
 export const routes: Routes = [
     { path: 'list', component: ListComponent },
   { path: 'form', component: FormComponent },
   { path: 'header', component: HeaderComponent },
+  { path: 'harry-potter', component: HarryPotterComponent },
   { path: '**', redirectTo: '' },
 ];
